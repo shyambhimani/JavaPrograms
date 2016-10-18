89d108aa0bd7d6b8a267490c353a96d3df8edb1f
Shyam Bhimani <shyambhimani@gmail.com>
2016-10-17 22:44:59 -0700

    AreaMethod class added

---
+++ Apex/src/oopsConcept/AreaMethod.java
@@ -0,0 +1,78 @@
+package oopsConcept;
+
+import java.util.*;
+
+public class AreaMethod {
+
+	static double pi = 3.14;
+	double radious;
+	double width;
+	double length;
+
+//	public AreaMethod(){
+//		
+//	}
+	public void areaOfCircle() {
+		double area = pi * (radious * radious);
+		System.out.println("Area of the Circle is: " + area);
+	}
+
+	public void areaOfSquare() {
+		double area = radious * radious;
+		System.out.println("Area of the Square is: " + area);
+	}
+
+	public void areaOfRectangle() {
+		double area = width * length;
+		System.out.println("Area of the Rectangle is: " + area);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AreaMethod area = new AreaMethod();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Please select the option below");
+		System.out.println("Enter 1 to find the Area of Circle");
+		System.out.println("Enter 2 to find the Area of Square");
+		System.out.println("Enter 3 to find the Area of Rectangle");
+		int option = sc.nextInt();
+
+		switch (option) {
+		case 1:
+			System.out.println("You have selected 1 to find Area of Circle");
+			System.out.println("Please enter Radious of Circle" );
+			area.radious = sc.nextDouble();
+			option = 1;
+			area.areaOfCircle();
+			break;
+
+		case 2:
+			System.out.println("You have selected 2 to find Area of Square");
+			System.out.println("Please enter length of side: " );
+			area.radious = sc.nextDouble();
+			option = 2;
+			area.areaOfSquare();
+			break;
+
+		case 3:
+			System.out.println("You have selected 3 to find Area of Rectangle");
+			System.out.println("Please enter length: " );
+			area.length = sc.nextDouble();
+			System.out.println("Please enter Width: ");
+			area.width = sc.nextDouble();
+			option = 3;
+			area.areaOfRectangle();
+			break;
+
+		default:
+			System.out.println("Ops! You have entred invalid input :( ");
+
+		}
+
+
+		sc.close();
+	}
+
+}
