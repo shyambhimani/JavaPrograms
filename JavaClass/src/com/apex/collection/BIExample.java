1783742c8ac92bce74de7c09197d4fd34970c9f4
Shyam Bhimani <shyambhimani@gmail.com>
2016-11-07 00:17:10 -0800

    class work added

---
+++ JavaClass/src/com/apex/collection/BIExample.java
@@ -0,0 +1,19 @@
+package com.apex.collection;
+
+class Student
+{
+ name;
+	Student(String n){
+		String name = n;
+	}
+}
+
+public class BIExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
