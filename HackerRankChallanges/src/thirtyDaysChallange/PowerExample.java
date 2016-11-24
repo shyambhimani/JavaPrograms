c31b853caf4d44917081125c9ea21052e99e6a24
Shyam Bhimani <shyambhimani@gmail.com>
2016-11-24 00:53:58 -0800

    up to date till day 18 challange

---
+++ HackerRankChallanges/src/thirtyDaysChallange/PowerExample.java
@@ -0,0 +1,18 @@
+package thirtyDaysChallange;
+
+public class PowerExample {
+
+	public static void main(String[] args) {
+		int n, p;
+		int power;
+		
+		n = 4;
+		p = 3;
+		// TODO Auto-generated method stub
+		
+		power = (int) Math.pow(n, p);
+		System.out.println(power);
+
+	}
+
+}
