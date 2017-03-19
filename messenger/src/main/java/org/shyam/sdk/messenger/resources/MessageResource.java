652cc43f95db2f2e86280e44a1c1b679a47787be
Shyam Bhimani <shyambhimani@gmail.com>
2017-03-18 18:09:46 -0700

    added REST functionality

--- messenger/src/main/java/org/shyam/sdk/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/shyam/sdk/messenger/resources/MessageResource.java
@@ -6,9 +6,15 @@ package org.shyam.sdk.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.shyam.sdk.messenger.model.Message;
@@ -18,11 +24,57 @@ import org.shyam.sdk.messenger.services.MessageService;
 public class MessageResource {
 	
 	MessageService messageService = new MessageService();
+	
+	
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	public List<Message> getMessages(){
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getMessages(@QueryParam("year") int year,
+									 @QueryParam("start") int start, 
+									 @QueryParam("size")int size){
+		if (year > 0){
+			return messageService.getAllMessagesForYear(year);
+		}
+		if (start >= 0 && size > 0){
+			return messageService.getAllPaginated(start, size);
+		}
 		return messageService.getAllMessages();
 	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return messageService.addMessage(message);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long messageId){
+		
+		return messageService.getMessage(messageId);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage (@PathParam("messageId") long messageId, Message message){
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void removeMessage(@PathParam("messageId") long messageId){
+		messageService.removeMessage(messageId);
+		
+	}
+	
+	
+	
 
 }
--- messenger/src/main/java/org/shyam/sdk/messenger/services/MessageService.java
+++ messenger/src/main/java/org/shyam/sdk/messenger/services/MessageService.java
@@ -5,21 +5,68 @@
 package org.shyam.sdk.messenger.services;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
+import org.shyam.sdk.messenger.database.DatabaseClass;
 import org.shyam.sdk.messenger.model.Message;
 
 public class MessageService {
 	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService(){
+		messages.put(1L, new Message(1, "Hello World", "Shyam"));
+		messages.put(2L, new Message(2, "This is REST", "Shyam"));
+
+	}
+	
 	public List<Message> getAllMessages(){
 		
-		Message m1 = new Message(1L, "Hello World!", "Shyam");
-		Message m2 = new Message(2L, "Jersey Here", "Shyam");
-		List<Message> list = new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		return list;
+		return new ArrayList<Message>(messages.values());
 		
+	}
+	
+	public List<Message> getAllMessagesForYear(int year){
+		List<Message> messageForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for(Message message : messages.values()){
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR) == year){
+				messageForYear.add(message);
+			}
+		}
+		
+		return messageForYear;
+	}
+	
+	
+	public List<Message> getAllPaginated(int start, int size){
+		ArrayList<Message> list = new ArrayList<Message>(messages.values());
+		if (start + size > list.size()) return new ArrayList<Message>();
+		return list.subList(start, start + size);
+	}
+	public Message addMessage(Message message){
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message getMessage(Long id){
+		return messages.get(id);
+	}
+	
+	public Message updateMessage(Message message){
+		if(message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id){
+		return messages.remove(id);
 	}
 
 }
